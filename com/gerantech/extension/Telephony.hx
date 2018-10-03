b1476a00ef9ede9cbfc41ca4f30eb4a08b6e6ffb
Mansour <mansurjavadi@gmail.com>
2018-10-03 11:07:02 +0330

    New JNI compatibility Formatted

--- com/gerantech/extension/Telephony.hx
+++ com/gerantech/extension/Telephony.hx
@@ -7,49 +7,42 @@ import com.gerantech.extension.enums.TelephonyState;
  * ...
  * @author Mansour Djawadi
  */
-
 #if cpp
 	import cpp.Lib;
 #elseif neko
 #end
-
 #if (android && openfl)
-	import openfl.utils.JNI;
+import lime.system.JNI;
 #end
 
-class Telephony
-{
-
+class Telephony {
 	#if (android && openfl)
 	private static var getIMEI_jni = JNI.createStaticMethod ("com.gerantech.extension.Telephony", "getIMEI", "()Ljava/lang/String;");
 	private static var addListener_jni = JNI.createStaticMethod("com.gerantech.extension.Telephony", "addListener", "(Lorg/haxe/lime/HaxeObject;)V", true);
 	#end
 
 	private static var _instance:Telephony;
+
 	private var callback:TelephonyState->Void;
 
-	public function new()
-	{
-	}
 
-	public function getIMEI():String
-	{
+	public function new() {}
+
+	public function getIMEI():String {
 		#if (android && openfl)
 		return getIMEI_jni();
 		#end
 		return null;
 	}
 
-	public function addCallback(callback:TelephonyState->Void):Void
-	{
+	public function addCallback(callback:TelephonyState->Void):Void {
 		this.callback = callback;
 		#if (android && openfl)
 		return addListener_jni([this]);
 		#end
 	}
 
-	public function onCallStateChanged(state:Int):Void
-	{
+	public function onCallStateChanged(state:Int):Void {
 		callback(cast(state, TelephonyState));
 	}
 
