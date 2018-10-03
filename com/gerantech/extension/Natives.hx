1b6c4954c5687af948949a4328b92ce10b106cb8
Mansour <mansurjavadi@gmail.com>
2018-10-03 10:30:44 +0330

    code fotmatted

--- com/gerantech/extension/Natives.hx
+++ com/gerantech/extension/Natives.hx
@@ -1,44 +1,37 @@
 package com.gerantech.extension;
 
 import com.gerantech.extension.enums.ToastDuration;
-
 #if cpp
-	import cpp.Lib;
+import cpp.Lib;
 #elseif neko
-	import neko.Lib;
+import neko.Lib;
 #end
-
 #if (android && openfl)
-	import lime.system.JNI;
+import lime.system.JNI;
 #end
 
-class Natives
-{
+class Natives {
 	#if (android && openfl)
-	private static var vibrate_jni = JNI.createStaticMethod ("com.gerantech.extension.Natives", "vibrate", "(I)V");
-	private static var wakeUp_jni = JNI.createStaticMethod ("com.gerantech.extension.Natives", "wakeUp", "()V");
-	private static var toast_jni = JNI.createStaticMethod ("com.gerantech.extension.Natives", "toast", "(Ljava/lang/String;I)V");
+	private static var vibrate_jni = JNI.createStaticMethod("com.gerantech.extension.Natives", "vibrate", "(I)V");
+	private static var wakeUp_jni = JNI.createStaticMethod("com.gerantech.extension.Natives", "wakeUp", "()V");
+	private static var toast_jni = JNI.createStaticMethod("com.gerantech.extension.Natives", "toast", "(Ljava/lang/String;I)V");
 	#end
 
-	public static function vibrate(inputValue:Int)
-	{
+	public static function vibrate(inputValue:Int) {
 		#if (android && openfl)
 		vibrate_jni(inputValue);
 		#end
 	}
 
-	public static function wakeUp():Void
-	{
+	public static function wakeUp():Void {
 		#if (android && openfl)
 		wakeUp_jni();
 		#end
 	}
 
-	public static function toast(text:String, duration:ToastDuration):Void
-	{
+	public static function toast(text:String, duration:ToastDuration):Void {
 		#if (android && openfl)
 		toast_jni(text, duration);
 		#end
 	}
-
 }
